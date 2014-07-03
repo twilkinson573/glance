9a19ad309413740435345c063713ae3cc6fa7f9c
Thomas <tuds_wilkinson@hotmail.com>
2014-07-03 15:03:34 +0100

    Implemented username functionality and added to views

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,6 @@ class Pin < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   do_not_validate_attachment_file_type :image
+  
+  validates :image, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   has_many :pins
+  
+  validates :username, presence: true
+
 end
