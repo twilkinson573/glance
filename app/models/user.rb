adc84b7e1c98387330470da608f634091064bf0c
Thomas <tuds_wilkinson@hotmail.com>
2014-07-03 19:37:55 +0100

    Fixed issue where edit/delete buttons would appear on objects without a user_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  has_many :pins
+  has_many :pins, dependent: :destroy
   
   validates :username, presence: true
 
